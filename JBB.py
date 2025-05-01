f51257da4d2f31e8affc70d2b8e5b9514cfaef24
Edgar Dobra <dobralaszloedgar@gmail.com>
2025-04-30 22:00:04 -0500

    H

--- JBB.py
+++ JBB.py
@@ -19,16 +19,12 @@ def run(protocol: protocol_api.ProtocolContext):
     aliquot_well_plates = protocol.load_labware('1.5ml_gpc_vial_aluminum_block', 11)
     PS_R = protocol.load_labware('wellplate_50ml_vial', 3)
     BB_R = protocol.load_labware('wellplate_20ml_vial', 6)
-    THF_r = protocol.load_labware('pyrex_reservoir_100ml', 8)
-    Lac_R = protocol.load_labware('jar_200ml', 1)
+    THF_r = protocol.load_labware('100ml_beaker_2', 8)       #change depending on what I use
+    Lac_R = protocol.load_labware('pyrex_reservoir_100ml', 1)
     Q_R = protocol.load_labware('20ml_reservoir', 2)
     LA_R = protocol.load_labware('20ml_reservoir', 5)
 
-
-    # Parameter List
     # LOCATIONS
-
-
     polymer_well_list = [stirplate_1['A1'], stirplate_1['A3'], stirplate_1['A5'], stirplate_1['A7'],
                          stirplate_1['B2'], stirplate_1['B4'], stirplate_1['B6'], stirplate_1['B8'],
                          stirplate_1['C1'],
@@ -72,29 +68,39 @@ def run(protocol: protocol_api.ProtocolContext):
     Livingness_Agent_reservoir = LA_R['A1']
 
     # VOLUMES
-    PS_Li = [2514.23, 1825.69, 1179.61, 2514.23, 1825.69, 1179.61, 2514.23, 1825.69, 1179.61, 2522.32, 1829.96, 1181.39, 2522.32, 1829.96, 1181.39, 2522.32, 1829.96, 1181.39, 2524.36, 1831.03, 1181.84, 2524.36, 1831.03, 1181.84, 2524.36, 1831.03, 1181.84, 2524.70, 1831.21, 1181.91, 2524.70, 1831.21, 1181.91, 2524.70, 1831.21, 1181.91]
-    BB = [992.69, 720.84, 465.75, 992.69, 720.84, 465.75, 992.69, 720.84, 465.75, 976.57, 708.50, 457.40, 976.57, 708.50, 457.40, 976.57, 708.50, 457.40, 972.52, 705.41, 455.31, 972.52, 705.41, 455.31, 972.52, 705.41, 455.31, 971.84, 704.90, 454.96, 971.84, 704.90, 454.96, 971.84, 704.90, 454.96]
-    Lactide = [2262.80, 3286.25, 4246.60, 2262.80, 3286.25, 4246.60, 2262.80, 3286.25, 4246.60, 2270.09, 3293.93, 4253.01, 2270.09, 3293.93, 4253.01, 2270.09, 3293.93, 4253.01, 2271.92, 3295.85, 4254.61, 2271.92, 3295.85, 4254.61, 2271.92, 3295.85, 4254.61, 2272.23, 3296.18, 4254.88, 2272.23, 3296.18, 4254.88, 2272.23, 3296.18, 4254.88]
-    Quencher = [230.28, 167.22, 108.04, 230.28, 167.22, 108.04, 230.28, 167.22, 108.04, 231.02, 167.61, 108.20, 231.02, 167.61, 108.20, 231.02, 167.61, 108.20, 231.21, 167.70, 108.24, 231.21, 167.70, 108.24, 231.21, 167.70, 108.24, 231.24, 167.72, 108.25, 231.24, 167.72, 108.25, 231.24, 167.72, 108.25]
-    Livingness_Agent = [302.12, 219.38, 141.75, 302.12, 219.38, 141.75, 302.12, 219.38, 141.75, 303.09, 219.89, 141.96, 303.09, 219.89, 141.96, 303.09, 219.89, 141.96, 303.33, 220.02, 142.01, 303.33, 220.02, 142.01, 303.33, 220.02, 142.01, 303.37, 220.04, 142.02, 303.37, 220.04, 142.02, 303.37, 220.04, 142.02]
-
+    PS_Li = [2661.82, 2225.04, 1675.26, 2632.07, 2194.36, 1646.67, 2613.40, 2175.17, 1628.88, 2670.74, 2231.28, 1678.79,
+             2641.04, 2200.59, 1650.17, 2622.39, 2181.39, 1632.37, 2672.98, 2232.84, 1679.68, 2643.30, 2202.15, 1651.05,
+             2624.65, 2182.96, 1633.25, 2233.10, 2233.10, 1679.82, 2643.67, 2202.41, 1651.20, 2625.03, 2183.22, 1633.40]
+    BB = [775.05, 647.87, 487.79, 787.59, 656.61, 492.73, 795.46, 662.07, 495.80, 762.56, 637.08, 479.33,
+            774.94, 645.70, 484.20, 782.71, 651.09, 487.22, 759.42, 634.37, 477.21, 771.76, 642.96, 482.06,
+            779.51, 648.33, 485.07, 633.92, 633.92, 476.86, 771.23, 642.50, 481.70, 778.98, 647.87, 484.71]
+    Lactide = [883.35, 1476.80, 2223.80, 897.64, 1496.72, 2246.31, 906.61, 1509.17, 2260.31, 886.31, 1480.93, 2228.48,
+                900.70, 1500.97, 2251.09, 909.73, 1513.49, 2265.15, 887.05, 1481.97, 2229.66, 901.46, 1502.03, 2252.29,
+                910.52, 1514.58, 2266.36, 1482.15, 1482.15, 2229.85, 901.59, 1502.21, 2252.49, 910.65, 1514.76, 2266.56]
+    Quencher = [179.79, 150.29, 113.15, 182.70, 152.32, 114.30, 184.53, 153.58, 115.01, 180.39, 150.71, 113.39,
+                183.32, 152.75, 114.54, 185.16, 154.02, 115.26, 180.54, 150.82, 113.45, 183.48, 152.86, 114.60,
+                185.32, 154.13, 115.32, 150.83, 150.83, 113.46, 183.50, 152.88, 114.61, 185.35, 154.15, 115.33]
+    Livingness_Agent = [235.88, 197.17, 148.45, 239.69, 199.83, 149.96, 242.09, 201.50, 150.89, 236.67, 197.73, 148.77,
+                        240.51, 200.40, 150.28, 242.92, 202.07, 151.21, 236.87, 197.86, 148.85, 240.72, 200.54, 150.36,
+                        243.13, 202.22, 151.30, 197.89, 197.89, 148.86, 240.75, 200.57, 150.37, 243.17, 202.24, 151.31]
 
     # Define Functions
-    def fill_aliquots(source, destination, volume=1000, bottom_clearance_aspirate=3, bottom_clearance_dispense=3):
+    def fill_aliquots(source, destination, volume=1000, bottom_clearance_aspirate=3, bottom_clearance_dispense=10):
         pipette.well_bottom_clearance.dispense = bottom_clearance_dispense
         pipette.well_bottom_clearance.aspirate = bottom_clearance_aspirate
-        pipette.distribute([volume] * len(destination), source, destination,
-                           disposal_volume=0)
+        pipette.distribute([volume] * len(destination), source, destination, disposal_volume=0)
+        pipette.distribute([50] * len(destination), Quencher_reservoir, destination, disposal_volume=0)
 
     def take_aliquot(source, destination, volume=60, mix_times=2, mix_volume=200, bottom_clearance_aspirate=1, bottom_clearance_dispense=3):
         pipette.well_bottom_clearance.dispense = bottom_clearance_dispense
         pipette.well_bottom_clearance.aspirate = bottom_clearance_aspirate
         pipette.transfer(volume, source, destination, mix_after=(mix_times, mix_volume), new_tip='always')
 
+    # Run starts here
     pipette.starting_tip = tiprack.well('A1')
 
     # Distribute PS-Li
-    pipette.well_bottom_clearance.aspirate = 3
+    pipette.well_bottom_clearance.aspirate = 1
     pipette.well_bottom_clearance.dispense = 50
     pipette.distribute(PS_Li[0:3]+PS_Li[9:12]+PS_Li[18:21]+PS_Li[27:30], PS_Li_reservoir_1, polymer_well_list[0:3]+
                        polymer_well_list[9:12]+polymer_well_list[18:21]+polymer_well_list[27:30], disposal_volume=0)
@@ -104,7 +110,7 @@ def run(protocol: protocol_api.ProtocolContext):
                        polymer_well_list[15:18]+polymer_well_list[24:27]+polymer_well_list[33:36], disposal_volume=0)
 
     # Add BB
-    pipette.well_bottom_clearance.aspirate = 2
+    pipette.well_bottom_clearance.aspirate = 1
     pipette.well_bottom_clearance.dispense = 50
     pipette.transfer(BB[0:9], BB_reservoir_1, polymer_well_list[0:9], disposal_volume=0)
     pipette.transfer(BB[9:18], BB_reservoir_2, polymer_well_list[9:18], disposal_volume=0)
@@ -112,7 +118,7 @@ def run(protocol: protocol_api.ProtocolContext):
     pipette.transfer(BB[27:36], BB_reservoir_4, polymer_well_list[27:36], disposal_volume=0)
 
     # Pause
-    protocol.pause("Aliquot fill stage next")
+    protocol.pause("Aliquot fill stage and Livingness Agent addition next, WAIT 60 minutes")
 
     # Fill aliquots with THF
     fill_aliquots(source=THF_reservoir, destination=aliquots_well_list)
@@ -121,22 +127,21 @@ def run(protocol: protocol_api.ProtocolContext):
     take_aliquot(source=polymer_well_list, destination=aliquots_well_list)
 
     # Add Livingness Agent
-    pipette.well_bottom_clearance.aspirate = 2
+    pipette.well_bottom_clearance.aspirate = 1
     pipette.well_bottom_clearance.dispense = 50
-    pipette.distribute(Livingness_Agent, Livingness_Agent_reservoir, polymer_well_list)
+    pipette.distribute(Livingness_Agent, Livingness_Agent_reservoir, polymer_well_list, disposal_volume=0) #Check if any gets split into 2
 
 
     # Pause
     protocol.move_labware(labware=tiprack, new_location=protocol_api.OFF_DECK)
     protocol.move_labware(labware=tiprack_2, new_location=9)
     pipette.starting_tip = tiprack_2.well('A1')
-    protocol.pause("Please replace pipette tips, and GPC vials, let Livingness Agent Stir")
+    protocol.pause("Let Livingness Agent stir for 30 minutes")
 
 
-    # Fill aliquots with THF
-    fill_aliquots(source=THF_reservoir, destination=aliquots_well_list)
 
-    # Add Lactide & Quench
+    '''
+    # Add Lactide & Quench V1
     bottom_clearance = 100-30
     counter = 0
     threshold_index = 1  # will go from 1 to 10
@@ -154,17 +159,25 @@ def run(protocol: protocol_api.ProtocolContext):
         pipette.well_bottom_clearance.aspirate = 2
         pipette.well_bottom_clearance.dispense = 50
         pipette.transfer(Quencher[i], Quencher_reservoir, polymer_well_list[i])
+    '''
+
+    # Add Lactide & Quench V2
+    pipette.well_bottom_clearance.dispense = 50
+    for i in range(len(polymer_well_list)):
+        pipette.well_bottom_clearance.aspirate = 2
+        pipette.transfer(Lactide[i], Lactide_reservoir, polymer_well_list[i])
+        # protocol.delay(seconds=30)
+        pipette.well_bottom_clearance.aspirate = 2
+        pipette.transfer(Quencher[i], Quencher_reservoir, polymer_well_list[i])
 
     # Pause
     protocol.move_labware(labware=tiprack_2, new_location=protocol_api.OFF_DECK)
     protocol.move_labware(labware=tiprack_3, new_location=9)
     pipette.starting_tip = tiprack_3.well('A1')
-    protocol.pause("Please make sure enough THF is present for aliquots")
+    protocol.pause("Please make sure enough THF is present for aliquots, GPC vials are ready, and replace tiprack")
 
     # Fill aliquots with THF
     fill_aliquots(source=THF_reservoir, destination=aliquots_well_list)
 
     # Take aliquots
     take_aliquot(source=polymer_well_list, destination=aliquots_well_list)
-
-
